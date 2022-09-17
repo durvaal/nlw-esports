072ec2a713cddb35719fcfd8b88c359df0f2f7bc
durvaal <plimayep@gmail.com>
2022-09-17 01:52:34 -0300

    chore: adjust style

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -41,7 +41,7 @@ function App() {
         Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui.
       </h1>
 
-      <div className="grid grid-cols-6 gap-6 mt-16">
+      <div className="grid grid-cols-3 gap-6 mt-16">
         {
           games.map((game) => {
             return (
