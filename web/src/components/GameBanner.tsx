73d18314526812f68c28144222fd26c1356fa2d0
durvaal <plimayep@gmail.com>
2022-09-17 02:21:57 -0300

    chore: add hover animation

--- web/src/components/GameBanner.tsx
+++ web/src/components/GameBanner.tsx
@@ -6,7 +6,7 @@ interface GameBannerProps {
 
 export function GameBanner(props: GameBannerProps) {
   return (
-    <a href="" className="relative rounded-lg overflow-hidden">
+    <a href="" className="relative rounded-lg overflow-hidden hover:scale-125 hover:z-10 ease-in duration-500">
       <img src={props.bannerUrl} alt="Game 1" />
 
       <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
